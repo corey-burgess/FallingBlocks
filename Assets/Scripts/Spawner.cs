8770cd3d7cb46525f492c024fffd9400cadc99db
Corey Burgess <coreyburgess@Coreys-MBP.lan>
2020-06-10 22:51:35 -0400

    Github Upload

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject fallingBlockPrefab;
+    public Vector2 secondsBetweenSpawnMinMax;
+    float nextspawnTime;
+
+    public Vector2 blockSizeMinMax;
+    public float spawnAngleMax;
+
+    Vector2 screenHalfSizeWorldUnits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenHalfSizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time > nextspawnTime)
+        {
+            float secondsBetweenSpawn = Mathf.Lerp(secondsBetweenSpawnMinMax.y, secondsBetweenSpawnMinMax.x, Difficulty.GetDifficultyPercent());
+            nextspawnTime = Time.time + secondsBetweenSpawn;
+
+            float spawnAngle = Random.Range(-spawnAngleMax, spawnAngleMax);
+            float spawnSize = Random.Range(blockSizeMinMax.x, blockSizeMinMax.y);
+            Vector2 spawnPosition = new Vector2(Random.Range(-screenHalfSizeWorldUnits.x, screenHalfSizeWorldUnits.x), screenHalfSizeWorldUnits.y + spawnSize);
+            GameObject newBlock = (GameObject) Instantiate(fallingBlockPrefab, spawnPosition, Quaternion.Euler(Vector3.forward * spawnAngle));
+            newBlock.transform.localScale = Vector2.one * spawnSize;
+        }
+
+        
+    }
+}
